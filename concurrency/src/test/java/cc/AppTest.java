95fbbfee98b104951c7c2d43fa38afae7b5802ea
zhangyunan <zyndev@gmail.com>
2024-10-10 19:46:01 +0800

    add concurrency module

---
+++ concurrency/src/test/java/cc/AppTest.java
@@ -0,0 +1,12 @@
+package cc;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void testTrue() {
+        Assertions.assertTrue(true);
+    }
+}
